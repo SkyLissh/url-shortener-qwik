a4eb9f5ace6831c787544f99c36d8bec657ba36b
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2024-03-04 21:07:12 -0600

    :bug: Fixed env variables
    Fixed environment variables for vercel and vite

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -2,15 +2,15 @@ export function getBaseUrl() {
   if (typeof window !== "undefined")
     // browser should use relative path
     return "";
-  if (process.env.VERCEL_ENV === "development")
-    return `http://localhost:${process.env.PORT ?? 5173}`;
+  if (import.meta.env.VITE_VERCEL_ENV === "development")
+    return `http://localhost:${import.meta.env.VITE_PORT ?? 5173}`;
 
-  if (process.env.VERCEL_URL)
+  if (import.meta.env.VITE_VERCEL_URL)
     // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    return `https://${import.meta.env.VITE_VERCEL_URL}`;
+  if (import.meta.env.VITE_RENDER_INTERNAL_HOSTNAME)
     // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+    return `http://${import.meta.env.VITE_RENDER_INTERNAL_HOSTNAME}:${import.meta.env.VITE_PORT}`;
   // assume localhost
-  return `http://localhost:${process.env.PORT ?? 5173}`;
+  return `http://localhost:${import.meta.env.VITE_PORT ?? 5173}`;
 }
